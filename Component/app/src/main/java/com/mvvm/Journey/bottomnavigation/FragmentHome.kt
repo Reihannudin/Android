dbeb73c3ac8cb9c4a34d3d4e4edb48fb04704658
Reihannudin <78741486+Reihannudin@users.noreply.github.com>
2022-04-26 18:19:05 +0700

    Delete Component directory

--- Component/app/src/main/java/com/mvvm/Journey/bottomnavigation/FragmentHome.kt
+++
@@ -1,8 +0,0 @@
-package com.mvvm.Journey.bottomnavigation
-
-import androidx.fragment.app.Fragment
-import com.mvvm.Journey.R
-
-class FragmentHome : Fragment (R.layout.fragment_home){
-
-}
--- Component/app/src/main/java/com/mvvm/Journey/bottomnavigation/FragmentMessage.kt
+++
@@ -1,8 +0,0 @@
-package com.mvvm.Journey.bottomnavigation
-
-import androidx.fragment.app.Fragment
-import com.mvvm.Journey.R
-
-class FragmentMessage : Fragment (R.layout.fragment_message){
-
-}
